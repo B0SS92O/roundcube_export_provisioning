3d903eb7c94ada0564fbdb9240b0ac39bc544960
B0SS92O <fawadqureshi2013@gmail.com>
2025-03-03 22:15:18 +0500

    Update export_provisioning.js
    Support for latest Roundcube theme.

--- export_provisioning.js
+++ export_provisioning.js
@@ -3,7 +3,7 @@
 if (window.rcmail) {
   rcmail.addEventListener('init', function(evt) {
     // <span id="settingstabdefault" class="tablink"><roundcube:button command="preferences" type="link" label="preferences" title="editpreferences" /></span>
-    var tab = $('<span>').attr('id', 'settingstabpluginexport_provisioning').addClass('tablink');
+    var tab = $('<li>').attr('id', 'settingstabpluginexport_provisioning').addClass('listitem provisioning');
 
     var button = $('<a>').attr('href', rcmail.env.comm_path+'&_action=plugin.export_provisioning').html(rcmail.gettext('export_provisioning', 'export_provisioning')).appendTo(tab);
     button.bind('click', function(e){ return rcmail.command('plugin.export_provisioning', this) });
